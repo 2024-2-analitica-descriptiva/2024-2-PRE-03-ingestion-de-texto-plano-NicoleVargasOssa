aba7de54835b5f47477dc8591090dff266801f49
NicoleVargasOssa <164451532+NicoleVargasOssa@users.noreply.github.com>
2024-12-27 01:07:52 -0500

    lab3_descrip

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,12 +1,41 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
-    """
+    
+    with open('files/input/clusters_report.txt', 'r', encoding='utf-8') as archivo:
+        lineas = archivo.readlines()
+
+    datos = []
+    contenido = lineas[4:]
+
+    fila = []
+
+    for linea in contenido:
+        dividir = linea.strip().split()
+
+        if dividir:
+            if not fila:
+                fila = [int(dividir[0]), int(dividir[1]), float(dividir[2].replace(',', '.')), " ".join(dividir[4:])]
+            else:
+                fila[3] += " " + " ".join(dividir)
+        else:
+            if fila:
+                fila[3] = fila[3].replace('.', '')
+                datos.append(fila[:4])
+                fila = []
+
+    columnas = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]
+    return pd.DataFrame(datos, columns=columnas)
+
+if __name__ == "__main__":
+    df_tabla = pregunta_01()
+    print(df_tabla)
+
+    
+"""
     Construya y retorne un dataframe de Pandas a partir del archivo
     'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
 
